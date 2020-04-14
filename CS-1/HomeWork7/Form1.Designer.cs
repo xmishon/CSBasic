9d028ef625b8cc293c9aa3c7b398cfb55fb09be9
Mikhail Zmeevskiy <xmishon17@gmail.com>
2020-04-14 23:49:36 +0300

    HomeWork7 stage 1

---
+++ CS-1/HomeWork7/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace HomeWork7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxField = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelFlagsCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxField
+            // 
+            this.pictureBoxField.Location = new System.Drawing.Point(12, 110);
+            this.pictureBoxField.Name = "pictureBoxField";
+            this.pictureBoxField.Size = new System.Drawing.Size(360, 360);
+            this.pictureBoxField.TabIndex = 0;
+            this.pictureBoxField.TabStop = false;
+            this.pictureBoxField.Click += new System.EventHandler(this.pictureBoxField_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(151, 32);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(53, 45);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Старт";
+            this.buttonStart.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X: , Y: ";
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelTime.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelTime.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelTime.Location = new System.Drawing.Point(242, 40);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(57, 31);
+            this.labelTime.TabIndex = 4;
+            this.labelTime.Text = "000";
+            // 
+            // labelFlagsCount
+            // 
+            this.labelFlagsCount.AutoSize = true;
+            this.labelFlagsCount.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.labelFlagsCount.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelFlagsCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFlagsCount.ForeColor = System.Drawing.Color.Cornsilk;
+            this.labelFlagsCount.Location = new System.Drawing.Point(56, 40);
+            this.labelFlagsCount.Name = "labelFlagsCount";
+            this.labelFlagsCount.Size = new System.Drawing.Size(57, 31);
+            this.labelFlagsCount.TabIndex = 5;
+            this.labelFlagsCount.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 576);
+            this.Controls.Add(this.labelFlagsCount);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBoxField);
+            this.Name = "Form1";
+            this.Text = "Сапёр";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxField;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelFlagsCount;
+    }
+}
+
---
+++ CS-1/HomeWork7/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork7
+{
+    public partial class Form1 : Form
+    {
+        private Cell[][] cells;
+        private Graphics graphics;
+        private int cellHeight;
+        private int cellWidth;
+        private int FIELD_SIZE = 8;
+        private bool isClickable;
+        Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCells();
+            graphics = pictureBoxField.CreateGraphics();
+            cellHeight = pictureBoxField.Height / FIELD_SIZE;
+            cellWidth = pictureBoxField.Width / FIELD_SIZE;
+            DrawField();
+            timer = new Timer();
+        }
+
+        private void DrawField()
+        {
+            Pen pen = new Pen(Color.Black);
+            pen.Width = 2;
+            Brush brush = new SolidBrush(Color.Gray);
+            graphics.FillRectangle(brush, new Rectangle(0, 0, pictureBoxField.Width - 1, pictureBoxField.Height - 1));
+            graphics.DrawRectangle(pen, new Rectangle(0, 0, pictureBoxField.Width - 1, pictureBoxField.Height - 1));
+            
+            for (int i = 0; i < FIELD_SIZE; i++)
+                graphics.DrawLine(pen, new Point(0, i * cellHeight), new Point(pictureBoxField.Height - 1, i * cellHeight));
+            for (int i = 0; i < FIELD_SIZE; i++)
+                graphics.DrawLine(pen, new Point(i * cellWidth, 0), new Point(i * cellWidth, pictureBoxField.Width - 1));
+        }
+
+        private void DrawCell(Brush brush, int x, int y, int minesCounter)
+        {
+            Rectangle rect = new Rectangle(x * cellWidth + 1, y * cellHeight + 1, cellHeight - 2, cellHeight - 2);
+            graphics.FillRectangle(brush, rect);
+            Brush brushF = new SolidBrush(Color.Black);
+            graphics.DrawString(minesCounter.ToString(), new Font("Calibri", 24), brushF, x * cellWidth + 9, y * cellHeight + 3);
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            isClickable = true;
+            DrawField();
+            InitializeCells();
+            timer.Stop();
+            try
+            {
+                timer.Tick -= TickerHandler;
+            } catch (Exception exc)
+            {
+                label1.Text = "Ошибка сборса таймера";
+            }
+            labelTime.Text = "000";
+            timer.Enabled = true;
+            timer.Interval = 1000;
+            timer.Start();
+            timer.Tick += TickerHandler;
+        }
+
+        private void TickerHandler(object sender, EventArgs e)
+        {
+            labelTime.Text = (int.Parse(labelTime.Text) + 1).ToString();
+            if (labelTime.Text.Length < 2) labelTime.Text = "00" + labelTime.Text;
+            if (labelTime.Text.Length < 3) labelTime.Text = "0" + labelTime.Text;
+        }
+
+        private struct Cell
+        {
+            public bool isMine;
+            public bool isFlag;
+            public int x;
+            public int y;
+            public bool isOpened;
+        }
+
+        private void InitializeCells()
+        {
+            cells = new Cell[FIELD_SIZE][];
+            for (int i = 0; i < cells.Length; i++)
+            {
+                cells[i] = new Cell[FIELD_SIZE];
+            }
+            for (int i = 0; i < FIELD_SIZE; i++)
+            {
+                for (int j = 0; j < FIELD_SIZE; j++)
+                {
+                    Cell cell = cells[i][j];
+                    cell.x = j;
+                    cell.y = i;
+                    cell.isMine = false;
+                    cell.isFlag = false;
+                    
+                }
+            }
+            Random r = new Random(); // Расставляем мины
+            for (int i = 0; i < 7; i++)
+            {
+                cells[r.Next(8)][r.Next(8)].isMine = true;
+            }
+        }
+
+        private void pictureBoxField_Click(object sender, EventArgs e)
+        {
+            if (isClickable)
+            {
+                Point point = pictureBoxField.PointToClient(Cursor.Position); // получили координаты клика
+                int x, y;
+                x = point.X / cellWidth;
+                y = point.Y / cellHeight;
+                label1.Text = "Ячейка: (" + x + ", " + y + ")";
+                OpenCell(x, y);
+            }
+
+        }
+
+        private void OpenCell(int x, int y)
+        {
+            Color color;
+            cells[x][y].isOpened = true;
+            if (cells[x][y].isMine)
+                color = Color.Red;
+            else
+                color = Color.LightGray;
+
+            int minesCounter = 0;
+            // Считаем количество мин в ячейках вокруг
+            checkCell(x - 1, y - 1, ref minesCounter);
+            checkCell(x, y - 1, ref minesCounter);
+            checkCell(x + 1, y - 1, ref minesCounter);
+            checkCell(x + 1, y, ref minesCounter);
+            checkCell(x + 1, y + 1, ref minesCounter);
+            checkCell(x, y + 1, ref minesCounter);
+            checkCell(x - 1, y + 1, ref minesCounter);
+            checkCell(x - 1, y, ref minesCounter);
+
+            DrawCell(new SolidBrush(color), x, y, minesCounter);
+        }
+
+        private void checkCell(int x, int y, ref int counter)
+        {
+            if (x > -1 && x < FIELD_SIZE && y > -1 && y < FIELD_SIZE)
+            {
+                if (cells[x][y].isMine)
+                    counter++;
+            }
+        }
+    }
+}
